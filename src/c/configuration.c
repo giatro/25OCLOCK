89cd926ef884e3eea5c1b40b45bcffbc3ef00112
Gianluca Troiani <gianluca.troiani@gmail.com>
2016-09-04 12:19:40 +0200

    font configuration works on pebble and pebble 2

--- src/c/configuration.c
+++ src/c/configuration.c
@@ -21,6 +21,9 @@ void set_style() {
   textaccentcolor = GColorWhite;
   
   int random = 0;
+  if(persist_exists(KEY_FONT_INDEX)) {
+    fontindex = persist_read_int(KEY_FONT_INDEX);
+  }
   #ifdef PBL_COLOR
   if(persist_exists(KEY_ST_COLOR_RED)) {
     int bgred   = persist_read_int(KEY_BG_COLOR_RED);
@@ -32,7 +35,6 @@ void set_style() {
     int ndred   = persist_read_int(KEY_ND_COLOR_RED);
     int ndgreen = persist_read_int(KEY_ND_COLOR_GREEN);
     int ndblue  = persist_read_int(KEY_ND_COLOR_BLUE);
-    fontindex = persist_read_int(KEY_FONT_INDEX);
     random  = persist_read_int(KEY_RANDOM);
     backgroundcolor  = GColorFromRGB(bgred, bggreen, bgblue);
     textcolor  = GColorFromRGB(stred, stgreen, stblue);
@@ -70,8 +72,11 @@ void inbox_received_callback(DictionaryIterator *iterator, void *context) {
   Tuple *color_nd_blue_t = dict_find(iterator, KEY_ND_COLOR_BLUE);
   Tuple *font_index_t = dict_find(iterator, KEY_FONT_INDEX);
   Tuple *random_t = dict_find(iterator, KEY_RANDOM);
-  
+    
   if(color_st_red_t) {
+    int fontindex  = font_index_t->value->int32;
+    time_font = fonts_load_custom_font(resource_get_handle(fonts[fontindex]));
+    persist_write_int(KEY_FONT_INDEX, fontindex);
     #ifdef PBL_COLOR
       int bgred   = color_bg_red_t->value->int32;
       int bggreen = color_bg_green_t->value->int32;
@@ -82,13 +87,10 @@ void inbox_received_callback(DictionaryIterator *iterator, void *context) {
       int ndred   = color_nd_red_t->value->int32;
       int ndgreen = color_nd_green_t->value->int32;
       int ndblue  = color_nd_blue_t->value->int32;
-      int fontindex  = font_index_t->value->int32;
       int random  = random_t->value->int32;;
       backgroundcolor = GColorFromRGB(bgred, bggreen, bgblue);
       textcolor = GColorFromRGB(stred, stgreen, stblue);
       textaccentcolor = GColorFromRGB(ndred, ndgreen, ndblue);
-      time_font = fonts_load_custom_font(resource_get_handle(fonts[fontindex]));
-    
     
       // Persist values
       persist_write_int(KEY_BG_COLOR_RED, bgred);
@@ -100,7 +102,6 @@ void inbox_received_callback(DictionaryIterator *iterator, void *context) {
       persist_write_int(KEY_ND_COLOR_RED, ndred);
       persist_write_int(KEY_ND_COLOR_GREEN, ndgreen);
       persist_write_int(KEY_ND_COLOR_BLUE, ndblue);
-      persist_write_int(KEY_FONT_INDEX, fontindex);
       persist_write_int(KEY_RANDOM, random);
     #endif
   }
